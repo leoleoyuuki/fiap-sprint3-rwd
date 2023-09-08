9c0657c8d45fd8d2cdcec4b8f3222b4a247c7af1
leoleoyuuki <rm550373@fiap.com.br>
2023-09-07 22:15:49 -0300

    correcoes footer e header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "../App.css"
+export default function Footer(props){
+    return(
+        <>
+            <footer>
+                <div className="footer">
+                    <div className='logo'><img src={props.logoPortoProps} alt="Logo Porto" /></div>
+                    <hr />
+                    <p>&copy; 2023 - Todos os direitos reservados</p>
+                </div>
+            </footer>
+        </>
+    )
+}
