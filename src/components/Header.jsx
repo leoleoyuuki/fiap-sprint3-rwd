78944e3292f38bc8425b0e7779b1c70a3fe946ec
gfrnca <gabrielbfranca27@gmail.com>
2023-09-10 18:47:10 -0300

    header centralizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
-
 import { Link } from 'react-router-dom'
 import '../App.css'
-export default function Header(props){
+export default function Header(props) {
+  return (
+    <>
+      <header>
+        <div className="head">
+          <div className="logo">
+            <img src={props.logoPortoProps} alt="Logo Porto" />
+          </div>
+          <nav>
+            <ul>
+              <li className="faq">FAQ</li>
+              <li className="alunos">
+                <Link to={'/alunos'}>Alunos</Link>
+              </li>
+              <div className="coberturas">
+                <li>Coberturas</li>
+              </div>
+            </ul>
+          </nav>
 
-    return(
-        <>
-            <header>
-                <div className="head">
-                    <div className='logo'><img src={props.logoPortoProps} alt="Logo Porto" /></div>
-                    <nav>
-                        <ul>
-                            <li className='faq'>FAQ</li>
-                            <li className='alunos'><Link to={"/alunos"}>Alunos</Link></li>
-                            <div className='coberturas'>
-                                <li>Coberturas</li>
-                            </div>
-                        </ul>
-                    </nav>
-                    <div className="menuMobile"><img src={props.menuProps} alt="menu mobile" /></div>
-                </div>
-            </header>
-        </>
-    )
+          <div className="menuMobile">
+            <img src={props.menuProps} alt="menu mobile" />
+          </div>
+          <div className="logo invisible">
+            <img src={props.logoPortoProps} alt="Logo Porto" />
+          </div>
+        </div>
+      </header>
+    </>
+  )
 }
--- src/components/Secao1.jsx
+++ src/components/Secao1.jsx
@@ -1,28 +1,27 @@
-import Cadeado from "../assets/cadeado.svg"
-import "../App.css"
-import {Link} from "react-router-dom";
+import Cadeado from '../assets/cadeado.svg'
+import '../App.css'
+import { Link } from 'react-router-dom'
 export default function Secao1() {
-    return (
-        <>
-
-        <section className="secao1">
-            <div className="container-secao1">
-                <div className="titulo">
-                    <h1>Pedale mais tranquilo com a segurança Porto</h1>
-                </div>
-                <div className="subtitulo">
-                    <img src={Cadeado} alt="cadeado"/>
-                    <p>Porto Seguro <span className="underscore">Bike</span></p>
-                </div>
-                <div className="botao" onClick={() => window.location.href = "/"}>
-                        <p>
-                            <Link to="/">Cotar Gratuitamente</Link>
-                        </p>
-                    </div>
-            </div>
-        </section>
-        </>
-    )
-
-
+  return (
+    <>
+      <section className="secao1">
+        <div className="container-secao1">
+          <div className="titulo">
+            <h1>Pedale mais tranquilo com a segurança Porto</h1>
+          </div>
+          <div className="subtitulo">
+            <img src={Cadeado} alt="cadeado" />
+            <p>
+              Porto Seguro <span className="underscore">Bike</span>
+            </p>
+          </div>
+          <div className="botao" onClick={() => (window.location.href = '/')}>
+            <p>
+              <Link to="/">Cotar Gratuitamente</Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
