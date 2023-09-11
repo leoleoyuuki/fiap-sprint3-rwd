e125b5de262c94b81e8291a842afe1a1e491ba45
gfrnca <gabrielbfranca27@gmail.com>
2023-09-11 08:54:26 -0300

    hotfixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom'
+import './index.css'
 import RouterConfig from './routes'
-=======
-import { createBrowserRouter , RouterProvider } from 'react-router-dom'
-import Alunos from './routes/Alunos/index.jsx'
-import App from './App'
-import Pagina from './routes/Pagina'
-
-
-
-let router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {path: '/',
-       element: <Pagina/>
-      },
-      
-      {path: '/alunos', 
-      element: <Alunos/>
-    }
-    ]
-  }
-])
->>>>>>> main
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
