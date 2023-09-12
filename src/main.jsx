ab5b3a5049c4d8ecc6f4f46f7af147ce1943cf7c
gfrnca <gabrielbfranca27@gmail.com>
2023-09-12 19:19:34 -0300

    scroll provider no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import RouterConfig from './routes'
+import { ScrollLockProvider } from './contexts/ScrollLockContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={RouterConfig} />
+    <ScrollLockProvider>
+      <RouterProvider router={RouterConfig} />
+    </ScrollLockProvider>
   </React.StrictMode>,
 )
