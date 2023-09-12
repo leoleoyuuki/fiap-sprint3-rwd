3ab27b136dc354a018fafd3149a106666053b5a4
leoleoyuuki <rm550373@fiap.com.br>
2023-09-12 13:30:31 -0300

    Footer add

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,14 @@
 import Header from '../components/Header'
 import Menu from '../assets/menu.svg'
+import Footer from '../components/Footer'
+import Logo from '../assets/logo.svg'
 
 export default function RouteWrapper(props) {
   return (
     <>
       <Header menuProps={Menu}/>
       {props.children}
+      <Footer logoPortoProps={Logo} />
     </>
   )
 }
