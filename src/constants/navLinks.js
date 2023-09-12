ae8a16a7940e13def43cef86bc50ddee9fdf20b6
gfrnca <gabrielbfranca27@gmail.com>
2023-09-12 19:20:57 -0300

    arquivo de navegação do mobile menu

---
+++ src/constants/navLinks.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    name: 'FAQ',
+    section: '.secao3',
+  },
+  {
+    name: 'Alunos',
+    path: '/alunos',
+  },
+  {
+    name: 'Coberturas',
+    section: '.secao2',
+  },
+]
