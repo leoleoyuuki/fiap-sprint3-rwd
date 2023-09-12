2e7d6737568ba601a795356189bef7b8b5440e34
gfrnca <gabrielbfranca27@gmail.com>
2023-09-12 19:19:54 -0300

    mudanças no header, funcao de abrir e fechar o menu mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Link } from 'react-router-dom'
 import Brand from '../Brand'
 import './styles.css'
+import { useEffect, useState } from 'react'
+import { HiMenu } from 'react-icons/hi'
+import MobileMenu from '../MobileMenu'
+import { useScrollLock } from '../../contexts/ScrollLockContext'
 
-export default function Header(props) {
+export default function Header() {
+  const url = window.location.pathname
 
-  const url = window.location.pathname;
-
-   const posicaoFAQ = document.querySelector('.secao3');
-   const posicaoCobertura = document.querySelector('.secao2');
-  const clickFAQ = ()=> {
-    
-    if(url === '/alunos'){
-      window.location.href = '/';
-      
-    }else if(url === '/'){
+  const posicaoFAQ = document.querySelector('.secao3')
+  const posicaoCobertura = document.querySelector('.secao2')
+  const clickFAQ = () => {
+    if (url === '/alunos') {
+      window.location.href = '/'
+    } else if (url === '/') {
       window.scrollTo({
         top: posicaoFAQ.offsetTop,
-        behavior: 'smooth'
-      });
-
+        behavior: 'smooth',
+      })
     }
   }
-  const clickCobertura = ()=> {
-    if(url === '/alunos'){
-      window.location.href = '/';
-     
-    }else if(url === '/'){
+  const clickCobertura = () => {
+    if (url === '/alunos') {
+      window.location.href = '/'
+    } else if (url === '/') {
       window.scrollTo({
         top: posicaoCobertura.offsetTop,
-        behavior: 'smooth'
-      });
-
+        behavior: 'smooth',
+      })
     }
   }
+
+  // Menu mobile
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  // Bloquear scroll ao abrir o menu
+  const { lockScroll, unlockScroll } = useScrollLock()
+
+  const handleOpenMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  useEffect(() => {
+    isMenuOpen ? lockScroll() : unlockScroll()
+  }, [isMenuOpen])
+
   return (
     <>
       <header>
@@ -41,8 +55,10 @@ export default function Header(props) {
             <Brand />
           </div>
           <nav>
-            <ul className='navUl'>
-              <li className="faq" onClick={clickFAQ}>FAQ</li>
+            <ul className="navUl">
+              <li className="faq" onClick={clickFAQ}>
+                FAQ
+              </li>
               <li className="alunos">
                 <Link to={'/alunos'}>Alunos</Link>
               </li>
@@ -52,14 +68,22 @@ export default function Header(props) {
             </ul>
           </nav>
 
-          <div className="menuMobile" >
-            <img src={props.menuProps} alt="menu mobile" />
+          <div
+            onClick={handleOpenMenu}
+            className={`menuMobile ${url !== '/' && 'accent'}`}
+          >
+            <HiMenu size={38} />
           </div>
+
           <div className="logo invisible">
             <img alt="" />
           </div>
         </div>
       </header>
+
+      {isMenuOpen && (
+        <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      )}
     </>
   )
 }
